c15ceef651a608f43fb341512f8c94a1da4c5b9e
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-07-12 13:48:46 +0200

    deploy command uploads everything in src/ folder. CSS styles file added, centering main div. Change log viewable in SPA. Angular added.

--- fabfile.py
+++ fabfile.py
@@ -2,21 +2,24 @@ import os
 import ftplib
 import webbrowser
 
-index_html = '/home/olof/prj/github/armorattackjs/src/index.html'
+src_path = '/home/olof/prj/github/armorattackjs/src/'
+index_html = os.path.join(src_path, 'index.html')
 
 
 def deploy():
-	user = os.environ['AA_USER']
-	pw = os.environ['AA_PW']
-	ftp = os.environ['AA_FTPURL']
-	print 'Opening FTP session.'
-	session = ftplib.FTP(ftp, user, pw)
-	file = open(index_html,'rb')
-	print 'Storing file(s) on remote server.'
-	session.storbinary('STOR index.html', file)
-	file.close()
-	session.quit()
+    user = os.environ['AA_USER']
+    pw = os.environ['AA_PW']
+    ftp = os.environ['AA_FTPURL']
+    print 'Opening FTP session.'
+    session = ftplib.FTP(ftp, user, pw)
+    for name in os.listdir(src_path):
+        path = os.path.join(src_path, name)
+        f = open(path, 'rb')
+        print 'Storing %s on remote server.' % name
+        session.storbinary('STOR %s' % name, f)
+        f.close()
+    session.quit()
 
 
 def run():
-	webbrowser.open(index_html)
+    webbrowser.open(index_html)
