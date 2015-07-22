056c6b9aa6df37ae39ec97daf8835d61cc2a8937
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-07-22 16:10:13 +0200

    Jeep steering logic implemented

--- src/jeep.js
+++ src/jeep.js
@@ -13,3 +13,66 @@ jeepPolys = function() {
 
 	return [frame, bonnet];
 }
+
+JeepFSM = function() {
+	var state = 'STILL';
+	var rotDir = 0;
+	var right = 0;
+	var left = 0;
+
+	var updateLeftRight = function(sig, par) {
+		if ( sig !== 'ControlDown' && sig !== 'ControlUp')
+			return;
+		if ( par !== 'left' && par !== 'right')
+			return;
+
+		var pressed = sig === 'ControlDown';
+		var isLeft = par === 'left';
+
+		if ( isLeft && pressed )
+			left = 1;
+
+		if ( !isLeft && pressed )
+			right = 1;
+
+		if ( isLeft && !pressed )
+			left = 0;
+
+		if ( !isLeft && !pressed )
+			right = 0;
+	};
+
+	var handleEvent = function(sig, par) {
+		switch(state) {
+
+			case 'STILL':
+			if ( sig === 'ControlDown' && par === 'gas')
+				state = 'FORWARD';
+			updateLeftRight(sig, par);
+			if ( (left + right) % 2 == 1) {
+				rotDir = left ? 1 : -1;
+				state = 'ROTATING';
+			}
+			break;
+
+			case 'ROTATING':
+			updateLeftRight(sig, par);
+			if ( (left + right) % 2 == 0) {
+				rotDir = 0;
+				state = 'STILL';
+			}
+			break;
+
+			case 'FORWARD':
+			if ( sig === 'ControlUp' && par === 'gas')
+				state = 'STILL';
+			break;
+		}
+
+	}
+	return {
+		handleEvent: handleEvent,
+		getState: function() { return state; },
+		getRotationSign: function() { return rotDir; }
+	};
+};
