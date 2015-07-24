25273ea4fe50c66054fe8b83886be16f11abedec
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-07-24 17:07:27 +0200

    Changelog

--- src/angular_script.js
+++ src/angular_script.js
@@ -12,19 +12,20 @@
 				{ version: 0.03, log: "Changelog added" },
 				{ version: 0.04, log: "FPS counter added" },
 				{ version: 0.05, log: "Changelog sorted by version" },
-				{ version: 0.06, log: "Removed random lines, added jeep polys"},
-				{ version: 0.07, log: "Added YouTube video to the side"},
-				{ version: 0.08, log: "Improved layout, changed colors, links"},
-				{ version: 0.09, log: "Jeep turnable"},
-				{ version: "0.10", log: "Added state dump for debugging purposes"}
+				{ version: 0.06, log: "Removed random lines, added jeep polys" },
+				{ version: 0.07, log: "Added YouTube video to the side" },
+				{ version: 0.08, log: "Improved layout, changed colors, links" },
+				{ version: 0.09, log: "Jeep turnable" },
+				{ version: "0.10", log: "Added state dump for debugging purposes" },
+				{ version: "0.11", log: "Play jeep fire sound via button" }
 				];
 				$scope.changelog = changelog;
 			}
 		};
 		var links = [
-			{ title: 'github', url: 'https://github.com/objarni/armorattackjs' },
-			{ title: 'trello', url: 'https://trello.com/b/59wlFBWQ/armor-attack-js-version' },
-			{ title: 'tests', url: 'tests.html' }
+		{ title: 'github', url: 'https://github.com/objarni/armorattackjs' },
+		{ title: 'trello', url: 'https://trello.com/b/59wlFBWQ/armor-attack-js-version' },
+		{ title: 'tests', url: 'tests.html' }
 		];
 		$scope.linecount = 10;
 		$scope.links = links;
